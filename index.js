f958524ff384821e13d52ce08d043fae8ad169a6
Camilo <camilobeltran174@gmail.com>
2023-11-09 14:43:28 -0500

    File renamed

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-
-    document.getElementById("btn-close-note").addEventListener("click", () => {
-        document.getElementById("note").style.display = "none";
-    });
-
-
-});
