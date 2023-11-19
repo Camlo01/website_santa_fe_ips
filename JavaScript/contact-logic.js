f39ffb33a8159238bf8547c795082f2c739eb4b0
Camilo <camilobeltran174@gmail.com>
2023-11-18 20:12:58 -0500

    Add functionality to the contact page

---
+++ JavaScript/contact-logic.js
@@ -0,0 +1,13 @@
+
+/**
+ * Function in charge to return to index page
+ */
+function returnToHome() {
+    window.location.href = './index.html';
+}
+
+
+function validateForm() {
+    // Si todas las validaciones pasan, el formulario se enviará
+    return false;
+}
